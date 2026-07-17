86e0302404df55275f513712722c64d3c198b79e
Ethan Waxman <ejw99@cornell.edu>
2026-07-17 14:08:42 -0400

    Encode/decode testing

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,4 +4,21 @@ from src.data import encode, decode
 def test_encode():
     s = "0123456789+= "
     result = [n for n in range(13)]
-    assert result == encode(s)
+    assert encode(s) == result
+
+
+def test_decode():
+    nums = [n for n in range(13)]
+    result = "0123456789+="
+    assert decode(nums) == result
+
+
+def test_decode_strips():
+    nums = [n for n in range(13)] + [12] * 5
+    result = "0123456789+="
+    assert decode(nums) == result
+
+
+def test_encode_decode_inverse():
+    s = "0123456789+= "
+    assert decode(encode(s)) == s[:-1]
