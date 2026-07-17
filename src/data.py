8eb79cba797bb721c84c62c2b8f1b4b74f48c766
Ethan Waxman <ejw99@cornell.edu>
2026-07-17 15:09:09 -0400

    Generate test data

--- src/data.py
+++ src/data.py
@@ -14,7 +14,6 @@ VOCAB_TO_ID = {
     "9": 9,
     "+": 10,
     "=": 11,
-    " ": 12,
 }
 
 ID_TO_VOCAB = {
@@ -30,7 +29,6 @@ ID_TO_VOCAB = {
     9: "9",
     10: "+",
     11: "=",
-    12: " ",
 }
 
 
@@ -40,15 +38,43 @@ def encode(s: str) -> list[int]:
 
 
 def decode(nums: list[int]) -> str:
-    """Convert token ids back to string and strip padding"""
-    return "".join([ID_TO_VOCAB[num] for num in nums]).strip()
+    """Convert token ids back to string"""
+    return "".join([ID_TO_VOCAB[num] for num in nums])
+
+
+def sample_problem(key: jax.Array, n_digits: int) -> tuple[int, int, int]:
+    """Generate a random triple [a, b, c] where a + b = c"""
+    assert n_digits >= 0, "n_digits must be greater than 0"
+    min_val = 0
+    max_val = 10 ** (n_digits)
+    a, b = jax.random.randint(key, shape=(2,), minval=min_val, maxval=max_val)
+    return a, b, a + b
+
+
+def format_problem(a: int, b: int, n_digits: int) -> str:
+    """Format the generated problem into a string and pad up to n_digits"""
+    assert a < 10**n_digits
+    assert b < 10**n_digits
+    return f"{a:0{n_digits}d}+{b:0{n_digits}d}="
+
+
+def format_solution(c: int, n_digits) -> str:
+    """Format a solution by padding to n_digits, reversing to improve model training"""
+    assert c < 10 ** (n_digits + 1)
+    return f"{c:0{n_digits + 1}d}"[::-1]
 
 
 def generate_data(
     key: jax.Array, n_digits: int = 3, batch_size: int = 16
 ) -> tuple[jax.Array, jax.Array]:
-    _, k1, k2 = jax.random.split(key, 3)
-    a = jax.random.randint(k1, shape=(batch_size, 1), minval=100, maxval=1000)
-    b = jax.random.randint(k2, shape=(batch_size, 1), minval=100, maxval=1000)
-    c = a + b
-    return jnp.concatenate([a, b], axis=1), c
+    """Generate batch_size sample problems"""
+    keys = jax.random.split(key, batch_size)
+    inputs = []
+    outputs = []
+
+    for k in keys:
+        a, b, c = sample_problem(k, n_digits)
+        inputs.append(encode(format_problem(a, b, n_digits)))
+        outputs.append(encode(format_solution(c, n_digits)))
+
+    return jnp.array(inputs), jnp.array(outputs)
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,24 +1,64 @@
-from src.data import encode, decode
+import jax
+from src.data import (
+    decode,
+    encode,
+    format_problem,
+    format_solution,
+    generate_data,
+    sample_problem,
+)
 
 
 def test_encode():
-    s = "0123456789+= "
-    result = [n for n in range(13)]
+    s = "0123456789+="
+    result = [n for n in range(12)]
     assert encode(s) == result
 
 
 def test_decode():
-    nums = [n for n in range(13)]
-    result = "0123456789+="
-    assert decode(nums) == result
-
-
-def test_decode_strips():
-    nums = [n for n in range(13)] + [12] * 5
+    nums = [n for n in range(12)]
     result = "0123456789+="
     assert decode(nums) == result
 
 
 def test_encode_decode_inverse():
-    s = "0123456789+= "
-    assert decode(encode(s)) == s[:-1]
+    s = "0123456789+="
+    assert decode(encode(s)) == s
+
+
+def test_sample_problem():
+    key = jax.random.PRNGKey(42)
+    a, b, c = sample_problem(key, 3)
+    assert 0 <= a < 1000
+    assert 0 <= b < 1000
+    assert 0 <= c < 1998
+    assert a + b == c
+
+
+def test_format_problem():
+    s = "123+456="
+    assert format_problem(123, 456, 3) == s
+
+
+def test_format_problem_pads():
+    s = "001+002="
+    assert format_problem(1, 2, 3) == s
+
+
+def test_format_solution():
+    s = "4321"
+    assert format_solution(1234, 3) == s
+
+
+def test_format_solution_pads():
+    s = "1000"
+    assert format_solution(1, 3) == s
+
+
+def test_generate_data():
+    key = jax.random.PRNGKey(42)
+    inputs, outputs = generate_data(key, 3, 16)
+    assert inputs.shape[0] == outputs.shape[0]
+    assert inputs.shape[0] == 16
+    assert inputs.shape[1] == 8
+    assert outputs.shape[1] == 4
